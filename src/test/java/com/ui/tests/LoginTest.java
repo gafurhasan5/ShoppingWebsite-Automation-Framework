120b90000c19a3b77dda1a21316399d82b8442c1
gafurhasa5 <gafurhasan5@gmail.com>
2026-02-08 23:57:40 +0530

    add changes

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -1,6 +1,8 @@
 package com.ui.tests;
 
- import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import static com.constants.Browser.*;
 import com.ui.pages.HomePage;
