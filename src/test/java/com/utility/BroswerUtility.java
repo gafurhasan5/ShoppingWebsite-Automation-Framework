3699271cb5bf76823496e9391db281392c38ffa5
gafurhasan5 <gafurhasan5@gmail.com>
2026-02-15 01:45:09 +0530

    Creat WebDriver threadlocal i.e BrowserUtility class is thread safe and i add screenshot functionality

--- src/test/java/com/utility/BroswerUtility.java
+++ src/test/java/com/utility/BroswerUtility.java
@@ -1,7 +1,13 @@
 package com.utility;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,29 +18,29 @@ import com.constants.Browser;
 
 public abstract class BroswerUtility {
 
-	private WebDriver driver;
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	Logger logger = LoggerUtility.getLogger(this.getClass());
 
 	public WebDriver getDriver() {
-		return driver;
+		return driver.get();
 	}
 
 	public BroswerUtility(WebDriver driver) {
 		super();
-		this.driver = driver; // create instance of driver variable
+		this.driver.set(driver);// create instance of driver variable
 	}
 
 	public BroswerUtility(Browser browserName) {
 		logger.info("Launching Browser for " + browserName);
 		if (browserName == Browser.CHROME) {
-
-			driver = new ChromeDriver();
+			driver.set(new ChromeDriver());
 		} else if (browserName == Browser.EDGE) {
 
-			driver = new EdgeDriver();
+			driver.set(new EdgeDriver());
 		} else if (browserName == Browser.FIREFOX) {
 
-			driver = new FirefoxDriver();
+			driver.set(new FirefoxDriver());
+
 		} else {
 			logger.error("Invalid Browser Name....Please select correct Browser");
 			System.err.print("Invalid Browser Name....Please select correct Browser");
@@ -43,34 +49,48 @@ public abstract class BroswerUtility {
 
 	public void goToWebSite(String URL) {
 		logger.info("Visiting the website" + URL);
-		driver.get(URL);
+		driver.get().get(URL);
 	}
 
 	public void maximizeWindow() {
 		logger.info("Maximizing the Browser window");
-		driver.manage().window().maximize();
+		driver.get().manage().window().maximize();
 	}
 
 	public void clickOn(By locator) {
 		logger.info("Finding Elements with locator" + locator);
-		WebElement element = driver.findElement(locator);
+		WebElement element = driver.get().findElement(locator);
 		logger.info("Elements Found and Now Performing click");
 		element.click();
 	}
 
 	public void enterText(By locator, String email) {
 		logger.info("Finding Elements with locator" + locator);
-		WebElement element = driver.findElement(locator);
+		WebElement element =driver.get().findElement(locator);
 		logger.info("Elements Found and Now Enter Text");
 		element.sendKeys(email);
 	}
 
 	public String getVisibilityText(By locator) {
 		logger.info("Finding Elements with locator" + locator);
-		WebElement element = driver.findElement(locator);
+		WebElement element = driver.get().findElement(locator);
 		logger.info("Elements Found and Now returning the visible" + element.getText());
 		return element.getText();
 
+	}
+
+	public String takeScreenShot(String name) {
+		TakesScreenshot screenShot = (TakesScreenshot) driver;
+		File scData = screenShot.getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "//screenshots//" + name;
+		File scFileDest = new File(path);
+		try {
+			FileUtils.copyFile(scData, scFileDest);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return path;
 	}
 
 }
