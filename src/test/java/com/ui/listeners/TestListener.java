6dd0750fd84dfe2a04df067234654637b1aac24b
gafurhasan5 <gafurhasan5@gmail.com>
2026-02-15 01:05:52 +0530

    Add thread safe(Thread-local)in extentReport for for safety for parallel test excecution

--- src/test/java/com/ui/listeners/TestListener.java
+++ src/test/java/com/ui/listeners/TestListener.java
@@ -11,6 +11,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.utility.ExtentReporterUtility;
 import com.utility.LoggerUtility;
 
 public class TestListener implements ITestListener {
@@ -23,36 +24,36 @@ public class TestListener implements ITestListener {
 		logger.info(result.getMethod().getMethodName());
 		logger.info(result.getMethod().getDescription());
 		logger.info(Arrays.toString(result.getMethod().getGroups()));
-		extentTest = extentReports.createTest(result.getMethod().getMethodName());
+		ExtentReporterUtility.createExtentTest(result.getMethod().getMethodName());
 	}
 
 	public void onTestSuccess(ITestResult result) {
 		logger.info(result.getMethod().getMethodName() + " " + "PASSED");
-		extentTest.log(Status.PASS, result.getMethod().getMethodName() + " " + "PASSED");
+		ExtentReporterUtility.getTest().log(Status.PASS, result.getMethod().getMethodName() + " " + "PASSED");
 	}
 
 	public void onTestFailure(ITestResult result) {
 		logger.error(result.getMethod().getMethodName() + " " + "FAILED");
-		extentTest.log(Status.FAIL, result.getMethod().getMethodName() + " " + "FAILED");
 		logger.error(result.getThrowable());
+		ExtentReporterUtility.getTest().log(Status.FAIL, result.getMethod().getMethodName() + " " + "FAILED");
+		ExtentReporterUtility.getTest().log(Status.FAIL,result.getThrowable());
+		
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		logger.warn(result.getMethod().getMethodName() + " " + "SKIPPED");
-		extentTest.log(Status.SKIP, result.getMethod().getMethodName() + " " + "SKIPPED");
+		ExtentReporterUtility.getTest().log(Status.SKIP, result.getMethod().getMethodName() + " " + "SKIPPED");
 
 	}
 
 	public void onStart(ITestContext context) {
 		logger.info("Test Suit Started");
-		extentSparkreporter = new ExtentSparkReporter(System.getProperty("user.dir") + "//report.html");
-		extentReports = new ExtentReports();
-		extentReports.attachReporter(extentSparkreporter);
+		ExtentReporterUtility.setupSparkReporter("report.html");
 	}
 
 	public void onFinish(ITestContext context) {
 		logger.info("Test Suit Started");
-		extentReports.flush();
+		ExtentReporterUtility.flushReport();
 	}
 
 }
---
+++ src/test/java/com/utility/ExtentReporterUtility.java
@@ -0,0 +1,30 @@
+package com.utility;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterUtility {
+	
+	private static ExtentReports extentReports;
+	private static ThreadLocal <ExtentTest> extentTest=new ThreadLocal<ExtentTest>();
+	public static void setupSparkReporter(String reportName)
+	{
+		ExtentSparkReporter extentSparkreporter = new ExtentSparkReporter(System.getProperty("user.dir")+reportName);
+		extentReports = new ExtentReports();
+		extentReports.attachReporter(extentSparkreporter);
+	}
+    public static void createExtentTest(String testName)
+    {
+    	ExtentTest Test= extentReports.createTest(testName);
+    	extentTest.set(Test);
+    }
+    public static ExtentTest getTest()
+    {
+    	return extentTest.get();
+    }
+    public static void flushReport()
+    {
+    	extentReports.flush();
+    }
+}
