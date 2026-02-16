6c5d400614d3be73efa64ff9ea3e67f780cdad3b
gafurhasan5 <gafurhasan5@gmail.com>
2026-02-16 12:30:18 +0530

    Use the @Parameter to pass value of browser islamdaTest and IsHeadless in testing.xml file

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
+import com.constants.Browser;
 import com.ui.pages.HomePage;
 import com.utility.BroswerUtility;
 import com.utility.LamdaTestUtility;
@@ -23,24 +25,25 @@ public class TestBase {
 //		homepage = new HomePage(CHROME,true);// true for isHeadless browser
 //	}
 	// forLamdaTest
-	private boolean isLamdaTest = true;
-	private boolean isHeadless = true;
-
+	private boolean isLamdaTest;
+	//private boolean isHeadless = true;
+    
+	@Parameters({"browser","isLamdaTest","isHeadless"})
 	@BeforeMethod(description = "load the HomePage of website")
-	public void setup(ITestResult result) {
+	public void setup(String browser,boolean isLamdaTest,boolean isHeadless ,ITestResult result) {
 
 		WebDriver lamdaDriver;
 
 		if (isLamdaTest) {
 
-			lamdaDriver = LamdaTestUtility.initializeLamdaTestSession("chrome", result.getMethod().getMethodName());
+			lamdaDriver = LamdaTestUtility.initializeLamdaTestSession(browser, result.getMethod().getMethodName());
 
 			homepage = new HomePage(lamdaDriver);
 
 		} else {
 
 			logger.info("load the HomePage of website");
-			homepage = new HomePage(CHROME, isHeadless);
+			homepage = new HomePage(Browser.valueOf(browser.toUpperCase()), isHeadless);
 		}
 	}
 
